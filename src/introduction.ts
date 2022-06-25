c060175ce7dda1efa3bbf637f4e957e3dfee6c4c
Leon Yalin <l.yalin@agentvi.com>
2022-06-25 10:39:15 +0300

    add protocols

--- src/introduction.ts
+++ src/introduction.ts
@@ -30,7 +30,7 @@ function diagramsAndEstimations() {
     - Capacity
       Type                 | Maximum effective capacity
       RDBMS                  3TB
-      Destributed Cache   ch   16GB - 128GB
+      Destributed Cache      16GB - 128GB
       NoSQL                  Depends
     `);
 }
@@ -48,7 +48,9 @@ function networks() {
     - Hepls distribute the content across different regions.
     - A cache for static assets
     - Reduces costs, decreases latency. Increases complexity
-    - Can be of 2 types: Push (you manually upload the content to CDN) and Pull (content will be cached only after first user requests it).
+    - Can be of 2 types:
+        Push - you manually upload the content to CDN
+        Pull - content will be cached only after first user requests it
     - Protects from DDOS attacks
     `);
 }
@@ -121,10 +123,102 @@ function queues() {
     `);
 }
 
+function protocols() {
+  logToHTML(`
+    Protocols:
+
+    TCP(Transmission Control Protocol)
+    - The protocol of the internet
+    - WebSocket, HTTP(REST, gRPC, GraplQL) based on TCP
+    + Connection-oriented, Reliable (3-way handshake: syn->syn/ack->ack, detect loss, retransmit), Ordered (ack after received), Error-checked (checksums)
+    - Slower than some other protocols
+
+    UDP (User Datagram Protocol)
+    - Does not wait for ACK or ensures ordering, possible packet loss
+    + Good for streaming data (video, gaming ect..)
+    + Built for speed
+
+    HTTP (Hypertext Transfer Protocol)
+    - Based on TCP
+    - Request/Responce Protocol
+    - Includes Status, Headers & Body
+    - Has GET, HEAD (same as Get, but only status & headers), POST, PUT, PATCH (partial update), DELETE, OPTIONS, CONNECT (establish a tunnel), TRACE (message loop-back test). 
+    - GET method has a limitation of the url lenght. In this case, we can use POST instead
+    - Status Codes:
+        100 - 199 Informational
+        200 - 299 Successful
+        300 - 399 Redirection
+        400 - 499 Client Error
+        500 - 599 Server Error
+    - Use only common codes: 200 OK, 201 Created, 400 Bad Request, 404 Not Found, 401 Not Authorized, 403 Forbidden, 500 Server Error
+    
+    REST (Representational State Transfer)
+    - A protocol, built on HTTP
+    - Uses headers, statuses & JSON as body format
+    - Client-Server, Cachable, Stateless, Layered
+    - RESTfullness: 
+        REST API that follows REST architecture
+        usage: GET - read, POST - create, PUT - update, DELETE - delete
+        resources: GET users/123123
+        actions: PUT users/123123/enable
+        pagination: GET users?page=1&pageSize=50&sortby=created
+    - API Best Practices:
+        Don't overcomplicate (simple is better)
+        Do less requests (nest data)
+        Be consistent in your API
+        Don't mutate data with GET
+        Use nouns and not verbs, use plural (GET /employees and not GET /getAllEmployees)
+        Use Versioning, e.g. /v1, /v2
+        Errors: use object and error code, e.g. return 400 {"errorMsg": "error description", code: 10030}
+        Authentication: use SSL, OAUTH or API key
+        Use DTO, not the full model (to avoid updating wrong fields)
+        Use validation.
+        Controller -> Request -> Vlidator -> Handling/Service
+        Documentation: Endpoints, Parameters, Schema, Formatting, Gotchas/Errors
+
+    WebSocket
+    - Protocol built on top of TCP. that maintains persistent connection between client and server
+    + Client and Server can exchange massages in both directions
+    - More complex to maintain, load balancers issues (websocket is a long-live connection)
+
+    Long Polling
+    - Is a technique that tries to solve polling with REST issues
+    - Instead of polling every X seconds, it polls once, then server waits until it has a new message/data.
+    - After that, server releases a REST call and returns a responce.
+    - Client receives the responce and immediately requests a new data
+
+    SOAP (Simple Object Access Protocol)
+    - A messaging protocol (similar to REST) between client and server, on top of HTTP
+    - Uses xml data, can be stateless or stateful (store data between calls)
+    - Best used in bank transacions. chains of messaging
+    - Document is built of root <envelope> element with <header> and <body> elements, and a <fault> element for errors
+    - Uses WSDL files (Web Service Description Language) to describe the available web service APIs
+
+    gRPC (Google Remote Procedure Call)
+    - PRC protol allows to invoke another service as if is a local function
+    - Uses protobuff and HTTP2
+    - Cannot be used in Browser
+
+    Protobuff (Protocol Buffers)
+    - A binary protocol, which is not human readable. More efficient than JSON
+    - Stores instructions in *.proto files. Not supported bu browsers
+    - Messages are smaller / faster than JSON/XML
+    - Great for communication between services
+
+    GraphQL (Graph Query Language)
+    - A protocol on top of HTTP, developed by Facebook in 2015, based on JSON
+    - Solves the problem of overfetching (too much data fetched) and underfetching (too little data, only ids for example)
+    - Lets you define which fields and entities to return
+    - uses 'Query' for GET, and 'Mutation' for POST, PUT, DELETE
+    - GraphQL uses POST requests, which are not cacheable, support outside the JS ecosystem is not great
+    `);
+}
+
 export default function introduction() {
   delimeterMsg('INTRODUCTION');
   logF(diagramsAndEstimations);
   logF(networks);
   logF(caching);
   logF(queues);
+  logF(protocols);
 }
